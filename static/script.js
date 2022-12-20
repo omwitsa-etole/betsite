5c636e1c8572e5179c98af4caa56bf8fc190c632
korg <omwitsa-etole@gmail.com>
2022-12-20 19:08:13 +0300

    4.1

--- static/script.js
+++ static/script.js
@@ -85,9 +85,9 @@ dragElement(document.getElementById("mydiv"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById("mydivheader")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById("mydivheader").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
